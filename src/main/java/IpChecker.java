e100f49e85ec3ce7d69cc6273ec16993799ccb94
tpxcer <tpxcer@outlook.com>
2019-08-03 11:30:38 +0800

    该用 Maven 包管理

--- src/main/java/IpChecker.java
+++ src/main/java/IpChecker.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+/**
+ * @author haseochen
+ */
 public class IpChecker {
 
     public static String getIp() throws Exception {
