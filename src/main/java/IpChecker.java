33a161fa1839627afa28ca14cd6aba4e0317b8f7
Haseo Chen <tpxcer@outlook.com>
2023-05-30 23:16:07 +0800

    调整ip获取地址

--- src/main/java/IpChecker.java
+++ src/main/java/IpChecker.java
@@ -7,14 +7,17 @@ import java.net.URL;
  * @author haseochen
  */
 public class IpChecker {
+    public static void main(String[] args) throws Exception {
+        getIp();
+    }
 
     public static String getIp() throws Exception {
-        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        URL whatismyip = new URL("https://ip.tool.lu");
         BufferedReader in = null;
         try {
             in = new BufferedReader(new InputStreamReader(
                     whatismyip.openStream()));
-            String ip = in.readLine();
+            String ip = in.readLine().split(": ")[1];
             return ip;
         } finally {
             if (in != null) {
